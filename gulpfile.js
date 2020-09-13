1ad545ab9aa9bd63b78709c50e2f575a982f176c
Donovan <donovant78@gmail.com>
2020-09-13 17:29:46 +0200

    Add headers to build lib with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,19 @@ var source = require('vinyl-source-stream')
 var buffer = require('vinyl-buffer')
 var rename = require('gulp-rename')
 var sourcemaps = require('gulp-sourcemaps')
+var injecter = require('gulp-inject-string')
 
 // Path setup
 const jsEntry = 'index.js'
 const jsSRC = './classes/'
 const jsDEST = './lib/'
 const jsFILES = [jsEntry]
+
+// File Header
+const { version, author, license } = require('./package.json')
+const d = new Date()
+const today = `${d.getFullYear()}/${d.getMonth().toString().padStart(2,'0')}/${d.getDate().toString().padStart(2,'0')}`
+const header = `MinesweeperJS v${version} | by ${author} | Under ${license} License | Date: ${today} | Find more : https://github.com/Dono7/MinesweeperJS`
 
 // Gulp task : JS
 function js(done) {
@@ -28,18 +35,22 @@ function js(done) {
         })
         .transform( babelify, { presets: ['@babel/preset-env'] } )
         .bundle()
-        .pipe(source( entry) )
-        .pipe( rename({ extname: '.min.js'}) )
+        .pipe( source( entry) )
+        .pipe( rename({ extname: `-${version}.min.js`}) )
         .pipe( buffer() )
-        .pipe( sourcemaps.init({ loadMaps: true }) )
+        // .pipe( sourcemaps.init({ loadMaps: false }) )
         .pipe( uglify() )
-        .pipe( sourcemaps.write('.') )
+        .pipe( injecter.prepend(`/* ${header} */\n`) )
+        .pipe( injecter.append(`\n`) )
+        // .pipe( sourcemaps.write('.') )
         .pipe( dest( jsDEST ))
     })
     done()
 }
 
+
 // Export tasks
 // Run it with `gulp js` or just `gulp`
 task('js',js)
 task('default', parallel(js))
+console.log(header)
