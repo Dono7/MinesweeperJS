15e4d0b769984fbc88a8f5d8cd0a74fbc1ef234a
Donovan <donovant78@gmail.com>
2020-09-15 01:18:31 +0200

    Set Checker Cell and Grid as global classes

--- classes/index.js
+++ classes/index.js
@@ -2,4 +2,7 @@ import Checker from './Checker.js'
 import Cell from './Cell.js'
 import Grid from './Grid.js'
 
-exports = { Grid, Cell }
+var global = window || global
+global.Checker = Checker
+global.Cell = Cell
+global.Grid = Grid
