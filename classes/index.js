56e0d6b33b2d504ae77c2c74f4af3b4f7c7fa83b
Donovan <donovant78@gmail.com>
2020-09-18 12:26:36 +0200

    Exports Grid

--- classes/index.js
+++ classes/index.js
@@ -2,7 +2,9 @@ import Checker from './Checker.js'
 import Cell from './Cell.js'
 import Grid from './Grid.js'
 
-var global = window || global
-global.Checker = Checker
-global.Cell = Cell
-global.Grid = Grid
+// var global = window || global
+// global.Checker = Checker
+// global.Cell = Cell
+// global.Grid = Grid
+
+exports = { Grid }
